4c67ed574a2c65caa980ddf7a01831b884d827ad
Antonio Hanania <antonioh@novint.loc>
2024-07-30 13:58:19 +0300

    next.config.js change prepare for deplyment

--- next.config.js
+++ next.config.js
@@ -1,15 +1,24 @@
-const path = require('path')
+// const path = require('path')
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   sassOptions: {
+//     includePaths: [path.join(__dirname, 'src', 'styles')],
+//   },
+//   env: {
+//     TELEGRAM_BOT_TOKEN: process.env.TELEGRAM_BOT_TOKEN,
+//     TELEGRAM_CHAT_ID: process.env.TELEGRAM_CHAT_ID,
+//   },
+// }
+
+// module.exports = nextConfig
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'src', 'styles')],
-  },
-  env: {
-    TELEGRAM_BOT_TOKEN: process.env.TELEGRAM_BOT_TOKEN,
-    TELEGRAM_CHAT_ID: process.env.TELEGRAM_CHAT_ID,
+  reactStrictMode: true,
+  experimental: {
+    serverActions: true,
   },
 }
 
 module.exports = nextConfig
-

da6f5dc4cc0b4386e1ec531db03d64e1709263ac
Antonio Hanania <antonioh@novint.loc>
2024-08-05 16:09:10 +0300

    added cache control in nextConfic

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,19 @@ const nextConfig = {
   env: {
     DATABASE_URL: process.env.DATABASE_URL,
   },
+  async headers() {
+    return [
+      {
+        source: "/",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "s-maxage=1, stale-while-revalidate=5",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
